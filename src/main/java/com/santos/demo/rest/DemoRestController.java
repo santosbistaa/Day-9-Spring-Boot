827bbbac178b7c40d60bd48c92006a809075c7ea
Santosh Bista <santosbista17@gmail.com>
2025-03-08 15:33:08 +0545

    Exception Handling

---
+++ src/main/java/com/santos/demo/rest/DemoRestController.java
@@ -0,0 +1,16 @@
+package com.santos.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+    //add code for Hello World
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+}
